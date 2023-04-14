a7a5c92d58d20d935e1c70eac97638e4116973da
Hector Martinez <hec.gre81@gmail.com>
2023-04-14 16:31:38 -0600

    date example

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,7 +1,13 @@
 function App() {
+  const date = new Date()
+  const a = 10
+  const b = 20
+  console.log(date.toString(), a + b)
   return (
     <div>
       <p>Hello World</p>
+      <p>My date is {date.toString()}</p>
+      <p>{a} + {b} = {a + b}</p>
     </div>
   )
 }
