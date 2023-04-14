a5eb62e367b4da4a9810804f66d1e08a469c3cc1
Hector Martinez <hec.gre81@gmail.com>
2023-04-14 17:56:01 -0600

    Add ex 1.2

--- part1/courseinfo/src/components/Content.jsx
+++ part1/courseinfo/src/components/Content.jsx
@@ -1,16 +1,21 @@
+import { Part } from "./Part"
+
 export const Content = (props) => {
   const {part1, part2, part3, exercises1, exercises2, exercises3} = props
   return (
     <>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part 
+        part={part1}
+        ex={exercises1}
+      />
+      <Part 
+        part={part2}
+        ex={exercises2}
+      />
+      <Part 
+        part={part3}
+        ex={exercises3}
+      />
     </>
   )
 }
