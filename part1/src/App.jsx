f3059163e434c697a81f38e5d46cdc8ec4174277
Hector Martinez <hec.gre81@gmail.com>
2023-04-14 17:31:05 -0600

    working with arrays

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,20 +1,29 @@
 function App() {
   const date = new Date()
   const a = 10
-  const b = 20
+  const b = 20 
   console.log(date.toString(), a + b)
+  const data = ['hola', 'adios']
   return (
     <div>
       <h1>Greetings</h1>
       <Hello name="Hector"/>
       <Hello />
+      <PrintData data={data} />
     </div>
   )
 }
 
 const Hello = ({name}) => {
+  console.log(name);
   return (
     <p>Hello World {name}</p>
+  )
+}
+
+const PrintData = (props) => {
+  return (
+    <p>{props.data}</p>
   )
 }
 
