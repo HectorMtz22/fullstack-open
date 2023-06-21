aec88816d5ec172ed8b654124f2d98d5304abc18
Hector Martinez <hec.gre81@gmail.com>
2023-06-21 17:00:33 -0600

    exercise 1.8: unicafe step 3

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -2,14 +2,17 @@ import PropTypes from "prop-types"
 import { Pair } from "./Pair"
 
 export const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good - bad) / all || 0
+  const positive = 100 * (good / (good + neutral + bad)) || 0
   return (
     <>
       <Pair title="Good" value={good} />
       <Pair title="Neutral" value={neutral} />
       <Pair title="Bad" value={bad} />
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad) || 0}</p>
-      <p>Positive {100 * (good / (good + neutral + bad)) || 0} %</p>
+      <p>All {all}</p>
+      <p>Average {average}</p>
+      <p>Positive {positive} %</p>
     </>
   )
 }
