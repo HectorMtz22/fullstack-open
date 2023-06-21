7904648981107a9425ac64c2f9ef2abaf5beecbc
Hector Martinez <hec.gre81@gmail.com>
2023-06-21 17:02:02 -0600

    exercise 1.9: unicafe step 4

--- part1/unicafe/src/components/Statistics.jsx
+++ part1/unicafe/src/components/Statistics.jsx
@@ -5,6 +5,8 @@ export const Statistics = ({good, neutral, bad}) => {
   const all = good + neutral + bad
   const average = (good - bad) / all || 0
   const positive = 100 * (good / (good + neutral + bad)) || 0
+
+  if (all === 0) return <p>No feedback given</p>
   return (
     <>
       <Pair title="Good" value={good} />
