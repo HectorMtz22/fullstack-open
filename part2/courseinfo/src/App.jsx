fe4fe95fc9b9e3486642c249b38dcb9c54f488b3
Hector Martinez <hec.gre81@gmail.com>
2023-06-28 18:48:18 -0600

    Add exercise 2.4: Course information step9

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -46,12 +46,17 @@ const App = () => {
     }
   ]
 
-  return courses.map((course) => (
-    <Course
-      key={course.id}
-      course={course} 
-    />
-  ))
+  return (
+    <>
+      <h1>Web Development Curriculum</h1>
+      {courses.map((course) => (
+        <Course
+          key={course.id}
+          course={course} 
+        />
+      ))}
+    </>
+  )
 }
 
 export default App
