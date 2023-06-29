dd67ba750e6a945084ba73c7c2e22067d2df49e6
Hector Martinez <hec.gre81@gmail.com>
2023-06-28 18:42:58 -0600

    Add exercise 2.3*: Course information step8

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,6 +1,8 @@
 export const Total = ({parts}) => {
-  let totalQuantity = 0
-  parts.forEach((part) => totalQuantity += part.exercises)
+  // * Old Method
+  // parts.forEach((part) => totalQuantity += part.exercises)
+
+  const totalQuantity = parts.reduce((accumulator, current) => accumulator + current.exercises, 0)
   return (
     <p>
       <b>Total of {totalQuantity} exercises</b>
