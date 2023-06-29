db323968aecd2bcae585caa1ff3667301a2d708a
Hector Martinez <hec.gre81@gmail.com>
2023-06-28 18:29:07 -0600

    Add exercise 2.1: Course information step6

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,8 +1,9 @@
 import { Part } from "./Part"
 
 export const Content = ({parts}) => {
-  return parts.map(({name, exercises}) => (
+  return parts.map(({name, exercises, id}) => (
     <Part 
+      key={id}
       part={name}
       ex={exercises}
     />
