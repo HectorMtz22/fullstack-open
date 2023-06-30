c982337c93f10d5ba6d3a9bcbd651a1b80848ff8
Hector Martinez <hec.gre81@gmail.com>
2023-06-29 18:20:54 -0600

    Add exercise 2.6: The Phonebook Step1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -6,19 +6,36 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("You submitted the form")
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+  }
+
+  const handleInputChange = (e) => {
+    console.log(e.target.value)
+    setNewName(e.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input 
+            value={newName}
+            onChange={handleInputChange}
+          />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
     </div>
   )
 }
