92badcbdb7de59aa2eec5c871bbce63152922a66
Hector Martinez <hec.gre81@gmail.com>
2023-07-03 20:13:04 -0600

    Delete testingData

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,15 +3,15 @@ import { Persons } from './components/Persons'
 import { PersonForm } from './components/PersonForm'
 import { Filter } from './components/Filter'
 
-const testingData = [
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-]
+// const testingData = [
+//     { name: 'Arto Hellas', number: '040-123456', id: 1 },
+//     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+//     { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+//     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+// ]
 
 const App = () => {
-  const [persons, setPersons] = useState(testingData) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
