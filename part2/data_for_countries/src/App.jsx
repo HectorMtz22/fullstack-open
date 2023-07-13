5607720ec4e504c7847af1c5d9f912a8f995222f
Hector Martinez <hec.gre81@gmail.com>
2023-07-12 18:38:46 -0600

    Add Search box

--- part2/data_for_countries/src/App.jsx
+++ part2/data_for_countries/src/App.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from 'react'
+import searchService from './services/search'
+
 function App() {
+  const [names, setNames] = useState(null)
+  const [search, setSearch] = useState('')
+
+  useEffect(() => {
+    searchService
+      .getAll()
+      .then(data => {
+        console.log("Fetched", data)
+        setNames(data)
+      })
+  }, [])
+  
 
   return (
     <>
+      <div>
+        <label htmlFor="searchbox">Find Countries </label>
+        <input 
+          type="text" 
+          id='searchbox' 
+          value={search} 
+          onChange={(e) => setSearch(e.target.value)} 
+        />
+      </div>
       <h1>Hello World</h1>
     </>
   )
