04d00f2331b893acadbd9e086325ddeffd52cf0f
Hector Martinez <hec.gre81@gmail.com>
2023-07-13 18:30:43 -0600

    Add documented object response

--- part2/data_for_countries/src/components/Country.jsx
+++ part2/data_for_countries/src/components/Country.jsx
@@ -1,6 +1,21 @@
 import { useEffect } from "react"
 import searchService from '../services/search'
 
+// interface country
+/*
+  {
+    name: String,
+    capital: String,
+    area, Int,
+    languages: String[],
+    flag: {
+      svg: String,
+      png: String,
+      alt: String
+    } 
+  }
+*/
+
 export const Country = ({name}) => {
   useEffect(() => {
     searchService
@@ -8,5 +23,16 @@ export const Country = ({name}) => {
       .then(country => console.log(country))
 
   })
-  return <p>Here is your country {name}</p>
+  return (
+    <>
+      <h2>Name</h2>
+      <p>Capital si</p>
+      <p>Area si</p>
+      <h3>Languages:</h3>
+      <ul>
+        <li>Si</li>
+        <li>Si</li>
+      </ul>
+    </>
+  )
 }
